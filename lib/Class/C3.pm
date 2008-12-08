5666758a53a84fd2269bf4ba07578840fff8333b
Florian Ragwitz <rafl@debian.org>
2008-12-08 04:15:46 +0000

    Import of FLORA/Class-C3-0.20 from CPAN.
    gitpan-cpan-distribution: Class-C3
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         FLORA/Class-C3-0.20.tar.gz
    gitpan-cpan-author:       FLORA
    gitpan-cpan-maturity:     released

--- lib/Class/C3.pm
+++ lib/Class/C3.pm
@@ -4,7 +4,7 @@ package Class::C3;
 use strict;
 use warnings;
 
-our $VERSION = '0.19';
+our $VERSION = '0.20';
 
 our $C3_IN_CORE;
 our $C3_XS;
@@ -67,6 +67,11 @@ sub import {
 
 ## initializers
 
+# This prevents silly warnings when Class::C3 is
+#  used explicitly along with MRO::Compat under 5.9.5+
+
+{ no warnings 'redefine';
+
 sub initialize {
     %next::METHOD_CACHE = ();
     # why bother if we don't have anything ...
@@ -99,6 +104,8 @@ sub uninitialize {
 }
 
 sub reinitialize { goto &initialize }
+
+} # end of "no warnings 'redefine'"
 
 ## functions for applying C3 to classes
 
